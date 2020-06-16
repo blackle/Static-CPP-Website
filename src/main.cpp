8f53c4159541d939063c5f4da86541ea58ccdf96
blackle <isabelle@blackle-mori.com>
2020-06-16 02:46:23 -0400

    More readable

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define UWS_HTTPRESPONSE_NO_WRITEMARK
 #include "App.h"
 #include "resources.h"
 #include <iostream>
@@ -5,29 +6,32 @@
 #define PORT 4873
 
 int main() {
+	//resource list is generated on construction of this class
 	Resources resources;
 
 	uWS::App().get("/*", [&resources](auto *res, auto *req) {
-			(void) res;
-			(void) req;
 			auto path = req->getUrl();
-			std::string modified;
+
+			std::string modified; //path is a string_view, so to modify it we need a temporary string that remains in scope.
 			if (path.back() == '/') {
 				modified = std::string(path) + "index.html";
 				path = modified;
 			}
-			path = path.substr(1, path.length());
+			path = path.substr(1, path.length()); //remove the / at the start of the path
+
 			auto i = resources.map.find(path);
-			if (i != resources.map.end()) {
-				auto resource = i->second;
-				res->writeHeader("Content-type", resource.mimetype);
-				res->writeHeader("ETag", resource.etag);
-				//todo: hhnnggg chunk it
-				res->end(std::string_view(resource.data, resource.length));
-			} else {
+			if (i == resources.map.end()) {
 				res->writeStatus("404 Not Found");
 		    res->end("404 Not Found");
-			}
+		    return;
+		  }
+
+			auto resource = i->second;
+			res->writeHeader("Content-type", resource.mimetype);
+			res->writeHeader("ETag", resource.etag);
+			//todo: hhnnggg chunk it
+			res->end(std::string_view(resource.data, resource.length));
+
 	}).listen(PORT, [](auto *token) {
 	    if (token) {
 		std::cout << "Listening on port " << PORT << std::endl;
